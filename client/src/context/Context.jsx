c9733daf4d375ec4a41034d58d2a2ed3eaca407d
Gagan Shukla <106883823+Gaganshukla01@users.noreply.github.com>
2025-06-10 22:52:54 +0530

    Update Context.jsx

--- client/src/context/Context.jsx
+++ client/src/context/Context.jsx
@@ -1,54 +1,58 @@
 import axios from "axios";
-import {
-    createContext,
-    useEffect,
-    useState
-} from "react";
-import {
-    toast
-} from "react-toastify";
-const backend_url = import.meta.env.VITE_BACKEND_URI
-export const AppContent = createContext() 
-    export const AppContextProvider = (props) => {
-    const [isLoggedIn, setIsLoggedin] = useState(false) 
-        const [userData, setUserData] = useState(false) 
-        const getAuthState = async () => {
-        
-        try {
-            axios.defaults.withCredentials = true
-            const {
-                data
-            } = await axios.get(`${backend_url}/api/auth/isAuthenticate`);
-            if (data.sucess) {
-                setIsLoggedin(true);
-                getUserData();
+import { createContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+const backend_url=import.meta.env.VITE_BACKEND_URI
+export const AppContent=createContext()
+
+
+export const  AppContextProvider=(props)=>{
+
+   
+    const[isLoggedIn,setIsLoggedin]=useState(false)
+    const[userData,setUserData]=useState(false)
+
+    const getAuthState=async()=>{
+            axios.defaults.withCredentials=true
+            try {
+                const {data} = await axios.get(`${backend_url}/api/auth/isAuthenticate`);
+              
+                if (data.sucess) {
+                    setIsLoggedin(true);
+                    getUserData();
+                }
+            } catch (error) {
+                console.log(error.response?.data?.message || "An error occurred");
             }
-        } catch (error) {
-            console.log(error.response?.data?.message || "An error occurred");
-        }
+            
+    
     }
-    const getUserData = async () => {
+
+    const getUserData=async()=>{
         try {
-            const {
-                data
-            } = await axios.get(`${backend_url}/api/user/data`) data.sucess ? setUserData(data.message) : 
-                toast.error(data.message)
+            const {data}= await axios.get(`${backend_url}/api/user/data`)
+            data.sucess?setUserData(data.message):toast.error(data.message)
+            
         } catch (error) {
             toast.error(data.message)
         }
     }
-    useEffect(() => {
+
+    useEffect(()=>{
         getAuthState()
-    }, []) const value = {
+    
+    },[])
+
+    const value={
         backend_url,
-        isLoggedIn,
-        setIsLoggedin,
-        userData,
-        setUserData,
+        isLoggedIn,setIsLoggedin,
+        userData,setUserData,
         getUserData
+
     }
-    return ({
-        props.children
-    })
+
+    return (
+        <AppContent.Provider value={value}>
+            {props.children}
+        </AppContent.Provider>
+    )
 }
-user unauthorised is coiming
